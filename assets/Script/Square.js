a8395ba6fb148b300f26dc16414b851ef13e3375
SaltyLeung <holyham@163.com>
2018-09-05 02:18:00 +0800

    FirstUpload

---
+++ assets/Script/Square.js
@@ -0,0 +1,78 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+var Square = cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        number: 0,  //所拥有的数字
+        numLabel: {
+            default: null,
+            type:cc.Label   
+        },
+        gameNode: {
+            default: null,
+            type: cc.Node
+        },
+        gameScript: {
+            default: null,
+            type: require("Game")
+        },
+        tempTarget:0,
+        toDouble: false,
+        toDestroyed: false,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    move() {
+        var action = cc.moveTo(0.5, this.gameScript.positionList[this.tempTarget]);
+// 执行动作
+        this.node.runAction(action);
+    },
+
+    onLoad () {
+        this.numLabel = this.node.getChildByName("Number");
+        this.gameScript = cc.find("Game").getComponent("Game");
+    },
+
+    start () {
+        
+        //console.log(this.numLabel.getComponent(cc.Label));
+        
+    },
+    down(currIndex) {
+        var targetIndex = currIndex;
+        for(var i = currIndex - 4; i > 0; i-= 4) 
+        if(gameScript.positionList[i].number == 0) targetIndex = i;
+        else if((this.number == this.gameScript.positionList[i].number) && this.gameScript.eatableList[i] == true)  {
+            this.gameScript.eat(currIndex, i, this.node);
+        }
+    },
+
+    update (dt) {
+        this.numLabel.getComponent(cc.Label).string = this.number.toString();
+    },
+});
+module.exports = Square;
---
+++ library/imports/69/69f1a3d7-39d6-47c6-9846-e7d1521efbdc.js
@@ -0,0 +1,199 @@
+"use strict";
+cc._RF.push(module, '69f1aPXOdZHxphG59FSHvvc', 'Game');
+// Script/Game.js
+
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+var Game = cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        squarePrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        positionList: { //不动，最下面一行为0 1 2 3 倒数第二行为 4 5 6 7 以此类推  具有子属性number
+            default: [],
+            type: [cc.Node]
+        },
+        eatableList: { //该节点位置是否可吞
+            default: [],
+            type: [cc.Boolean]
+        },
+        moveableList: { //管理可移动方框
+            default: [],
+            type: [cc.Node]
+        }
+
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {
+        for (var i = 0; i < 16; ++i) {
+            this.positionList[i].number = 0;
+            this.moveableList[i] = null;
+            this.eatableList[i] = false;
+        }
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    },
+    start: function start() {
+        this.randomCreate();
+        //console.log("aaaaa");
+    },
+    update: function update(dt) {},
+    randomCreate: function randomCreate() {
+        //找随机空位
+        var emptyPos = new Array();
+        for (var i = 0; i < 16; ++i) {
+            if (this.positionList[i].number == 0) emptyPos.push(i);
+        }if (emptyPos.length == 0) this.gameOver();
+        var index = Math.floor(Math.random() * emptyPos.length);
+
+        //随机生成2或4
+        var newValue = (1 + Math.floor(Math.random() * 2)) * 2;
+
+        var newSquare = cc.instantiate(this.squarePrefab);
+        //console.log(newSquare);
+        newSquare.setPosition(this.positionList[emptyPos[index]].getPosition());
+
+        newSquare.getComponent(require("Square")).number = newValue;
+        newSquare.getComponent(require("Square")).gameScript = this;
+        //newSquare.setGlobalZOrder(1000);
+        this.node.addChild(newSquare);
+
+        this.positionList[emptyPos[index]].number = newValue;
+        this.moveableList[emptyPos[index]] = newSquare;
+        console.log("----------");
+        //console.log(emptyPos[index]);
+        for (var i = 0; i < 16; ++i) {
+            if (this.moveableList[i] != null) console.log("moveableNum" + i + ": " + this.moveableList[i].getComponent(require("Square")).number);
+        }
+        for (var i = 0; i < 16; ++i) {
+            console.log("position" + i + ": " + this.positionList[i].number);
+        }
+    },
+    moveDown: function moveDown() {
+        /*for(var i = 0; i < 16; ++i) {
+            this.eatableList[i] = true;
+        }
+        for(var i = 4; i < 16; ++i) {
+            if(this.moveableList[i] != null) this.moveableList[i].down(i);
+        }*/ //标记已经合并的并且挪出空位
+        for (var i = 0; i < 4; ++i) {
+            //列
+            for (var j = i; j < 16; j += 4) {
+                //每列中每个
+                if (this.moveableList[j] == null) continue;
+                var hasHole = [false, false, false, false];
+                var hasMerge = [false, false, false, false];
+                var targetIndex = j;
+                for (var k = j - 4; k >= 0; k -= 4) {
+                    //计算目标
+                    if (this.positionList[k].number == 0) {
+                        targetIndex = k;hasHole[Math.floor(j / 4)] = true;continue;
+                    } else if (hasHole[Math.floor(k / 4)] == true) {
+                        targetIndex = k;hasHole[Math.floor(j / 4)] = true;continue;
+                    } else if (this.positionList[k].number == this.positionList[j].number && hasMerge[Math.floor(k / 4)] == false) {
+                        targetIndex = k;
+                        hasHole[Math.floor(j / 4)] = true;
+                        hasMerge[Math.floor(k / 4)] = true;
+                        this.moveableList[j].getComponent(require("Square")).toDestroyed = true;
+                        this.moveableList[k].getComponent(require("Square")).toDouble = true;
+                        break;
+                    } else break;
+                }
+                //console.log(i+ " " +j +"targetIndex "+targetIndex);
+                this.moveableList[j].getComponent(require("Square")).tempTarget = targetIndex;
+            }
+        }
+
+        for (var i = 4; i < 16; ++i) {
+            if (this.moveableList[i] != null) this.moveableList[i].getComponent(require("Square")).move();
+        } //物理移动（逻辑上还没移动），移动后下标为source，target为目标
+        for (var i = 0; i < 16; ++i) {
+            if (this.moveableList[i] != null && this.moveableList[i].getComponent(require("Square")).toDestroyed == true) {
+                console.log("Destroy" + i);var temp = this.moveableList[i];setTimeout(function () {
+                    temp.destroy();
+                }, 0.5);this.moveableList[i] = null;this.positionList[i].number = 0;
+            }
+        } //删除多余卡片 
+
+        var tempList = new Array();
+        for (var j = 0; j < 16; ++j) {
+            tempList[j] = this.moveableList[j];
+        } //for(var j = 0; j < 16; ++j)console.log("TempList"+j+" : "+tempList[j]);
+        //for(var j = 0; j < 16; ++j)console.Log
+        for (var j = 0; j < 16; ++j) {
+            this.moveableList[j] = null;
+        }for (var i = 0; i < 16; ++i) {
+            if (tempList[i] != null) {
+                console.log("TYpe" + _typeof(tempList[i].getComponent(require("Square")).tempTarget));this.moveableList[tempList[i].getComponent(require("Square")).tempTarget] = tempList[i];
+            }
+        } //moveableList复位
+        //for(var j = 0; j < 16; ++j)console.log("mList(aftertemp)"+j+" : "+this.moveableList[j]);
+        for (var i = 0; i < 16; ++i) {
+            if (this.moveableList[i] == null) this.positionList[i].number = 0;else if (this.moveableList[i].getComponent(require("Square")).toDouble == true) {
+                this.moveableList[i].getComponent(require("Square")).number *= 2;
+                this.positionList[i].number = this.moveableList[i].getComponent(require("Square")).number;
+                this.moveableList[i].getComponent(require("Square")).toDouble = false;
+            } else this.positionList[i].number = this.moveableList[i].getComponent(require("Square")).number;
+        } //维护positionList
+    },
+    onKeyDown: function onKeyDown(event) {
+        // set a flag when key pressed
+        switch (event.keyCode) {
+            case cc.KEY.s:
+                this.moveDown();
+                this.randomCreate();
+                //for(var i = 0; i < 16; ++i) console.log(this.moveableList[i]);
+                break;
+        }
+    },
+    eat: function eat(source, target, newNode) {
+        this.moveableList[target].destroy();
+        this.moveableList[target] = this.moveableList[source];
+        this.moveableList[source] = null;
+
+        this.eatableList[target] = false;
+
+        this.positionList[source].number = 0;
+        this.positionList[target].number *= 2;
+    },
+    gameOver: function gameOver() {}
+
+    // update (dt) {},
+
+});
+
+module.exports = Game;
+
+cc._RF.pop();
